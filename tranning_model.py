2f31cb78bfd5d626558896243740f4ebc040d785
unknown <huyvt>
2024-09-13 09:32:34 +0700

    train and save model

--- tranning_model.py
+++ tranning_model.py
@@ -3,10 +3,13 @@ import pandas as pd
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
 df = read_csv('cleaned_data.csv')
-X = df[['dien_tich', 'so_phong', 'nam_xay_dung']]
+X = df[['Diện tích', 'Mặt tiền', 'Số phòng ngủ', 'Số toilet', 'Số tầng']]
 y = df['gia']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -14,17 +17,66 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 def train_and_save_LinearRegression(X_train, y_train):
     model = LinearRegression()
     model.fit(X_train, y_train)
+    
     y_pred = model.predict(X_test)
-    # Tính MSE
     mse = mean_squared_error(y_test, y_pred)
-    # Tính R-squared
-    r2 = r2_score(y_test, y_pred)
-
-    print(f"MSE: {mse}")
-    print(f"R-squared: {r2}")
+    r2_score = model.score(X_test, y_test)
+    print(f"Mean Squared Error: {mse}")
+    print(f"R^2 Score: {r2_score}")
     print('#' * 50)
+    
     joblib.dump(model, 'linear_regression_model.pkl')
     print("SAVE linear_regression_model")
 
-def train_and_save_LinearRegression(X_train, y_train):
+def train_and_save_RandomForestRegressor(X_train, y_train):
+    model = RandomForestRegressor(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
 
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    r2_score = model.score(X_test, y_test)
+    print(f"Mean Squared Error: {mse}")
+    print(f"R^2 Score: {r2_score}")
+    print('#' * 50)
+    
+    joblib.dump(model, 'random_forest_model.pkl')
+    print("SAVE random_forest_model")
+    
+def train_and_save_GradientBoostingRegressor(X_train, y_train):
+    model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, random_state=42)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    r2_score = model.score(X_test, y_test)
+    print(f"Mean Squared Error: {mse}")
+    print(f"R^2 Score: {r2_score}")
+    print('#' * 50)
+    
+    joblib.dump(model, 'gradient_boosting_model.pkl')
+    print("SAVE gradient_boosting_model")
+    
+    
+def train_and_save_KNeighborsRegressor(X_train, y_train):
+    model = KNeighborsRegressor(n_neighbors=5)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    r2_score = model.score(X_test, y_test)
+    print(f"Mean Squared Error: {mse}")
+    print(f"R^2 Score: {r2_score}")
+    print('#' * 50)
+
+    joblib.dump(model, 'knn_model.pkl')
+    print("SAVE gradient_bknn_modeloosting_model")
+    
+
+def main():
+    train_and_save_LinearRegression()
+    train_and_save_RandomForestRegressor()
+    train_and_save_GradientBoostingRegressor()
+    train_and_save_KNeighborsRegressor()
+
+if __name__ == "__main__":
+    main()
